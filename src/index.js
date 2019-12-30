e0c9f5cb676ec0edcbc0f21ab366041df2f322bf
Jesus Ramirez <jesusdevelp@gmail.com>
2019-12-30 16:19:59 -0600

    Vimos que es JSX

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@
 
 // container.appendChild(element);
 
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 const element = <h1>Hello, Platzi Badges!</h1>;
 
-const container = document.getElementById('app');
+const container = document.getElementById("app");
 
 // ReactDOM.render(__qué__, __dónde__);
 ReactDOM.render(element, container);
