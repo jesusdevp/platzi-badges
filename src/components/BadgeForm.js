9bee404808144896651ed986a82ea73a77d80d94
Jesus Ramirez <jesusdevelp@gmail.com>
2020-01-07 14:23:57 -0600

    Se manejaron los estados del formulario

--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -1,10 +1,15 @@
 import React from "react";
 
 class BadgeForm extends React.Component {
+  state = {};
   handleChange = e => {
-    console.log({
-      name: e.target.name,
-      value: e.target.value
+    // console.log({
+    //   name: e.target.name,
+    //   value: e.target.value
+    // });
+
+    this.setState({
+      [e.target.name]: e.target.value
     });
   };
 
@@ -15,6 +20,7 @@ class BadgeForm extends React.Component {
   handleSubmit = e => {
     e.preventDefault();
     console.log("Form was submutted");
+    console.log(this.state);
   };
 
   render() {
@@ -30,6 +36,51 @@ class BadgeForm extends React.Component {
               className="form-control"
               type="text"
               name="firstName"
+              value={this.state.firstName}
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Last Name</label>
+            <input
+              onChange={this.handleChange}
+              className="form-control"
+              type="text"
+              name="lastName"
+              value={this.state.lastName}
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Email</label>
+            <input
+              onChange={this.handleChange}
+              className="form-control"
+              type="email"
+              name="email"
+              value={this.state.email}
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Job Title</label>
+            <input
+              onChange={this.handleChange}
+              className="form-control"
+              type="text"
+              name="jobTitle"
+              value={this.state.jobTitle}
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Twitter</label>
+            <input
+              onChange={this.handleChange}
+              className="form-control"
+              type="text"
+              name="twitter"
+              value={this.state.twitter}
             />
           </div>
 
