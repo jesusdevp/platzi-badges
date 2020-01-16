95cd00d71c066e487f559c34e55aba0698407436
Jesus Ramirez <jesusdevelp@gmail.com>
2020-01-16 13:20:27 -0600

    Se manejaron las actualizaciones automaticas

--- src/pages/Badges.js
+++ src/pages/Badges.js
@@ -7,6 +7,7 @@ import confLogo from "../images/badge-header.svg";
 import BadgesList from "../components/BadgesList";
 import PageLoading from "../components/PageLoading";
 import PageError from "../components/PageError";
+import MiniLoader from "../components/MiniLoader";
 import api from "../api";
 
 class Badges extends React.Component {
@@ -18,6 +19,12 @@ class Badges extends React.Component {
 
   componentDidMount() {
     this.fetchData();
+
+    this.intervalId = setInterval(this.fetchData, 5000);
+  }
+
+  componentWillMount() {
+    clearInterval(this.intervalId);
   }
 
   fetchData = async () => {
@@ -32,7 +39,7 @@ class Badges extends React.Component {
   };
 
   render() {
-    if (this.state.loading === true) {
+    if (this.state.loading === true && !this.state.data) {
       return <PageLoading />;
     }
 
@@ -62,6 +69,7 @@ class Badges extends React.Component {
           </div>
 
           <BadgesList badges={this.state.data} />
+          {this.state.loading && <MiniLoader />}
         </div>
       </React.Fragment>
     );
